9e37244ca44beb8021fda17fce17226c283cc04e
JPCabral <cabral.j@unoesc.edu.br>
2024-07-16 10:20:47 -0300

    alterado stages

--- data/stages.lua
+++ data/stages.lua
@@ -1,88 +1,111 @@
--- Minlevel and multiplier are MANDATORY
--- Maxlevel is OPTIONAL, but is considered infinite by default
--- Create a stage with minlevel 1 and no maxlevel to disable stages
+-- Definir os estágios de experiência, habilidades e níveis mágicos
 experienceStages = {
 	{
 		minlevel = 1,
 		maxlevel = 8,
-		multiplier = 7,
+		multiplier = 50,
 	},
 	{
 		minlevel = 9,
-		maxlevel = 20,
-		multiplier = 6,
-	},
-	{
-		minlevel = 21,
 		maxlevel = 50,
-		multiplier = 5,
+		multiplier = 80,
 	},
 	{
 		minlevel = 51,
 		maxlevel = 100,
-		multiplier = 4,
+		multiplier = 60,
 	},
 	{
 		minlevel = 101,
-		multiplier = 2,
+		maxlevel = 150,
+		multiplier = 40,
+	},
+	{
+		minlevel = 151,
+		maxlevel = 200,
+		multiplier = 30,
+	},
+	{
+		minlevel = 201,
+		maxlevel = 300,
+		multiplier = 15,
+	},
+	{
+		minlevel = 301,
+		maxlevel = 400,
+		multiplier = 12,
+	},
+	{
+		minlevel = 401,
+		maxlevel = 500,
+		multiplier = 10,
+	},
+	{
+		minlevel = 501,
+		maxlevel = 600,
+		multiplier = 5,
+	},
+	{
+		minlevel = 601,
+		multiplier = 1,
 	},
 }
 
 skillsStages = {
 	{
-		minlevel = 10,
-		maxlevel = 60,
-		multiplier = 15,
-	},
-	{
-		minlevel = 61,
+		minlevel = 1,
 		maxlevel = 80,
 		multiplier = 10,
 	},
 	{
 		minlevel = 81,
-		maxlevel = 110,
-		multiplier = 6,
+		maxlevel = 100,
+		multiplier = 7,
 	},
 	{
-		minlevel = 111,
-		maxlevel = 125,
+		minlevel = 101,
+		maxlevel = 120,
 		multiplier = 4,
 	},
 	{
-		minlevel = 126,
+		minlevel = 121,
+		maxlevel = 300,
 		multiplier = 2,
+	},
+	{
+		minlevel = 301,
+		multiplier = 1,
 	},
 }
 
 magicLevelStages = {
 	{
 		minlevel = 0,
-		maxlevel = 60,
-		multiplier = 10,
-	},
-	{
-		minlevel = 61,
 		maxlevel = 80,
-		multiplier = 7,
+		multiplier = 10,
 	},
 	{
 		minlevel = 81,
 		maxlevel = 100,
-		multiplier = 5,
+		multiplier = 7,
 	},
 	{
 		minlevel = 101,
-		maxlevel = 110,
+		maxlevel = 120,
 		multiplier = 4,
 	},
 	{
-		minlevel = 111,
-		maxlevel = 125,
+		minlevel = 121,
+		maxlevel = 130,
 		multiplier = 3,
 	},
 	{
-		minlevel = 126,
+		minlevel = 131,
+		maxlevel = 300,
 		multiplier = 2,
+	},
+	{
+		minlevel = 301,
+		multiplier = 1,
 	},
 }
